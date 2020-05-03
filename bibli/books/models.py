9ac301a15a8fd8f6c2235d36441c549fe52df942
Thibaud Hulot <thulot@m-c02sp1pfgtdx.home>
2020-05-03 13:59:01 +0200

    added books first static view

--- bibli/books/models.py
+++ bibli/books/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    genre = models.CharField(max_length=20)
+    year = models.IntegerField
+
--- bibli/books/views.py
+++ bibli/books/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
 
-# Create your views here.
+def table(request):
+    return render(request, 'books/table.html')
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the books index.")

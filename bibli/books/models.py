d891828661f60cf785db1d25d06a289a6883c879
Tibino7 <thibaud.hulot@gmail.com>
2021-01-22 10:36:09 +0100

    edited authors to be a list of chars

--- bibli/books/models.py
+++ bibli/books/models.py
@@ -1,18 +1,10 @@
 from djongo import models
 
-class Author(models.Model):
-    name = models.CharField(max_length=255)
-
-    class Meta:
-        abstract = True
-
 class Book(models.Model):
     isbn13 = models.CharField(max_length=13, primary_key=True)
     isbn10 = models.CharField(max_length=10, null=True, blank=True)
     title = models.CharField(max_length=255)
-    authors = models.ArrayField(
-        model_container = Author,
-    )
+    authors = models.JSONField()
     genre = models.CharField(max_length=50)
     year = models.IntegerField(null=True, blank=True)
     language = models.CharField(max_length=2)
